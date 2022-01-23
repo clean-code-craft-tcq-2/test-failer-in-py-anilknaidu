eb6241e5fec502d40a0674e58cf42c388f70bb8d
Naidu Anil Kumar (RBEI/EPV32-EC) <AnilKumar.Naidu@in.bosch.com>
2022-01-23 15:24:17 +0530

    Test failer update

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,38 @@
 
 def print_color_map():
+    textFilePrint = open("PrintedColorMapManual.txt",'w')
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+            textToBePrinted = f'{i * 5 + j} | {major} | {minor}'
+            print(textToBePrinted)
+            textFilePrint.write(textToBePrinted + '\n')
+    textFilePrint.close()
     return len(major_colors) * len(minor_colors)
 
 
-result = print_color_map()
-assert(result == 25)
+import unittest
+class colorMapManualTester(unittest.TestCase):
+    ColorMapPrintedManual = open("PrintedColorMapManual.txt",'r')
+    ColorMapPrintedManualText = ColorMapPrintedManual.readlines()
+    result = print_color_map()
+    def test_color_map_manual_length(self):
+        self.assertEqual(self.result,len(self.ColorMapPrintedManualText))
+
+    def test_color_map_manual_index(self):
+        testIndex = 0
+        for color_map_manual_index in range(len(self.ColorMapPrintedManualText)):
+            self.assertEqual(int(self.ColorMapPrintedManualText[color_map_manual_index].split("|")[0].strip()),testIndex)
+            testIndex +=1
+    
+    def test_color_map_manual_alignment(self):
+        max_alignmnet_expected = len(str(self.result)) +1
+        for color_map_manual_index in range(len(self.ColorMapPrintedManualText)):
+            self.assertEqual(int(len(self.ColorMapPrintedManualText[color_map_manual_index].split("|")[0])),max_alignmnet_expected)
+        
+if __name__ == "__main__":
+    unittest.main()
+    colorMapManualTester.ColorMapPrintedManual.close()
+
 print("All is well (maybe!)\n")
--- tshirts.py
+++ tshirts.py
@@ -1,4 +1,3 @@
-
 def size(cms):
     if cms < 38:
         return 'S'
@@ -7,8 +6,25 @@ def size(cms):
     else:
         return 'L'
 
+smallShirtSizeList =range(34,38)
+mediumShirtSizeList =range(38,42)
+largeShirtSizeList =range(42,46)
+shirtSizeTestRange = range(30,50)
+#Test Code below
+import unittest
+class ShirtSizeTester(unittest.TestCase):
+    def test_ShirtSize(self):
+        print("Executing Shirt Size tests")
+        for shirtSize in shirtSizeTestRange:
+            if(shirtSize in smallShirtSizeList):
+                self.assertEqual(size(shirtSize),'S')
+            elif(shirtSize in mediumShirtSizeList):
+                self.assertEqual(size(shirtSize),'M')
+            elif(shirtSize in largeShirtSizeList):
+                self.assertEqual(size(shirtSize),'L')
+            else:
+                self.assertEqual(size(shirtSize),'InvalidShirtSize')
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
-assert(size(43) == 'L')
+if __name__ == "__main__":
+    unittest.main()
 print("All is well (maybe!)\n")
